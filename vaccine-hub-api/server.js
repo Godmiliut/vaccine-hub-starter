876032f9e7cceb12f1611d7806f1160360210374
Godmiliut <supermiliut72@gmail.com>
2022-06-28 14:49:47 -0400

    Step 5 done

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { PORT } = require("./config")
+const authRouter = require("./routes/auth")
 
 const { NotFoundError } = require("./utils/errors")
 
@@ -11,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use(morgan("tiny"))
+
+app.use("/auth", authRouter)
 
 app.use((req, res, next) =>{
     return next(new NotFoundError())
@@ -23,8 +27,6 @@ app.use((error, req, res, next) =>{
         error: {message, status}
     })
 })
-
-const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () =>{
     console.log(`🚀 Server running http://localhost:${PORT}`)
